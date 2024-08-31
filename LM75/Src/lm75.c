544ffc83978a1cbab6237e31eca25384b3bb7b0f
Radek Rogocki <lupus5523@gmail.com>
2024-08-31 22:59:35 +0200

    Feat: Add function to write to configuration register

--- LM75/Src/lm75.c
+++ LM75/Src/lm75.c
@@ -29,3 +29,27 @@
 #define TWO_FAULTS          0x08
 #define FOUR_FAULTS         0x10
 #define SIX_FAULTS          0x18
+
+
+/* Register lengths */
+#define MAX_REG_SIZE        2
+#define MIN_REG_SIZE        1
+
+
+/* Maximum transmission time */
+#define TIMEOUT             500
+
+
+static LM75_STATUS write_config(LM75 *dev, uint8_t mem_addr, uint8_t *data);
+
+
+/* Write to configuration register */
+static LM75_STATUS write_config(LM75 *dev, uint8_t mem_addr, uint8_t *data)
+{
+    if (HAL_OK != HAL_I2C_Mem_Write(dev->i2c, dev->addr, mem_addr, I2C_MEMADD_SIZE_8BIT, data, MIN_REG_SIZE, TIMEOUT))
+    {
+        return LM75_ERROR;
+    }
+
+    return LM75_OK;
+}
